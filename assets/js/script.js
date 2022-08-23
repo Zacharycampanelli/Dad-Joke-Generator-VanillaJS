49e48718213dab69540dc15e95390fea008eaf6f
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-08-23 16:41:56 -0400

    updated code to display joke on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const jokeContainer = document.getElementById('container');
 const jokeButton = document.getElementById('joke-btn');
-
+const messageBody = document.querySelector('message-body');
 const getJoke = async () => {
   const url = 'https://icanhazdadjoke.com/';
   try {
@@ -8,7 +8,10 @@ const getJoke = async () => {
 
     let data = await response.json();
     clearContainer();
-    appendJoke(data.joke);
+    let joke = splitJoke(data.joke);
+    // let dividedJoke = formatJoke(joke);
+    console.log(joke)
+    appendJoke(joke);
   } catch (error) {
     console.log(error);
   }
@@ -20,11 +23,53 @@ const clearContainer = () => {
     }
 }
 
+// const formatJoke = (opening, punchLine) => {
+//    console.log('shit')
+//   return [opening, punchLine]
+// }
+
 const appendJoke = (apiJoke) => {
 
-    const joke = document.createTextNode(apiJoke);
-    joke.innerHTML = apiJoke;
+    const joke = document.createElement("p");
+    console.log('here')
+   joke.textContent = apiJoke;
+    joke.classList.add('has-text-danger-dark')
     jokeContainer.appendChild(joke);
+}
+
+const splitJoke = (joke) => {
+  console.log(joke)
+  // if(joke.includes(':')) {
+  //   console.log('skip')
+  //   getJoke()
+  // }
+  
+   let newJoke = joke.split(
+    
+   )
+
+  if(newJoke[newJoke.length - 1] === '') {console.log("pop" + newJoke.pop())};
+  if(newJoke.length > 2) { 
+    getJoke()
+  }
+  console.log(newJoke);
+  // if(newJoke.length === 2 //|| (newJoke.length === 3 && newJoke[2] === '')//|| newJoke[1] !== undefined 
+  //   ){
+  //   console.log('here')
+  // let opening = newJoke[0];
+
+  // let punchLine = newJoke[1]
+  // const formattedJoke = formatJoke(opening, punchLine)// [opening, punchLine]
+  // return formattedJoke
+  // }
+  // else {
+  //   // if (newJoke.includes('')) 
+  //   // console.log('undefined')
+  //   // else console.log('too many')
+  //   // return newJoke
+  //    getJoke();
+  // }
+  return newJoke
 }
 
 jokeButton.addEventListener("click", getJoke)
