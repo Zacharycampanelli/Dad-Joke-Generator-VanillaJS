dcccdfc80caa877a3c129367aaa210e06daa2606
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-08-24 22:52:15 -0400

    started styling page based off of mobile-first design

--- assets/js/saved.js
+++ assets/js/saved.js
@@ -3,11 +3,9 @@ const jokeContainer = document.getElementById('saved');
 
 function getSavedJokes() {
   for (let [key, value] of Object.entries(localStorage)) {
-    console.log(value);
-    newValue = value + "\n"
-    let item = document.createTextNode(newValue);
+    
+    let item = document.createTextNode(value);
     let br = document.createElement("br");
-    console.log(`${item}`);
     jokeContainer.appendChild(item);
     jokeContainer.appendChild(br);
 
