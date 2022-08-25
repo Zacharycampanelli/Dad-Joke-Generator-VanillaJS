326cdeb68cb7d2644b4a4deac87e0a0739c13c55
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-08-24 21:14:41 -0400

    created saved jokes pages that loads the saved jokes from localstorage onto the page

--- assets/js/saved.js
+++ assets/js/saved.js
@@ -1,43 +1,17 @@
-const jokeContainer = document.querySelector('container');
+const jokeContainer = document.getElementById('saved');
 
-const loadJokes = () => {
-  // for (item in localStorage) {
-  //     let vari = JSON.parse(window.localStorage.getItem(item))
-  //     console.log(vari)
-  // }
-  
-  const items = { ...localStorage };
-  console.log(items);
-  console.log(items.length);
-  for (item in items) console.log(items[item]);
-//   let joke = document.createElement('p');
-//   joke.textContent = items[item];
-  jokeContainer.appendChild(items[item]);
-};
 
-function allStorage() {
+function getSavedJokes() {
+  for (let [key, value] of Object.entries(localStorage)) {
+    console.log(value);
+    newValue = value + "\n"
+    let item = document.createTextNode(newValue);
+    let br = document.createElement("br");
+    console.log(`${item}`);
+    jokeContainer.appendChild(item);
+    jokeContainer.appendChild(br);
 
-    var values = [],
-        keys = Object.keys(localStorage),
-        i = keys.length;
-
-    while ( i-- ) {
-        values.push( localStorage.getItem(keys[i]) );
-        let storageItem = localStorage.getItem(keys[i])
-        console.log(storageItem);
-        console.log(jokeContainer)
-    }
-    for(value in values)
-    console.log(values[value]);
-    jokeContainer.appendChild(values);
-    return values;
+   
+  }
 }
-
-function allStorage2() {
-    for (let [key, value] of Object.entries(localStorage)) {
-        console.log(`${value}`);
-        jokeContainer.appendChild(value);
-    }
-}
-allStorage2();
-//loadJokes();
+getSavedJokes();
