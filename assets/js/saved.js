43dcd0b302c5f84291a16e1f8e3bdd6519e6ab80
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-08-28 18:46:43 -0400

    fixed homepage script so liked jokes are saved to localstorage as an array

--- assets/js/saved.js
+++ assets/js/saved.js
@@ -1,17 +1,87 @@
 const jokeContainer = document.getElementById('saved');
+let jokeId = 0;
+const jokes = []
 
+// function getSavedJokes() {
+//   // for (let [key, value] of Object.entries(localStorage)) {
+//   let item = document.createElement('li');
+
+//   item.innerText = value;
+//   item.className = 'saved-joke';
+//   item.setAttribute('id', key);
+//   let deleteButton = document.createElement('button');
+//   deleteButton.innerHTML = 'Delete';
+
+//   let br = document.createElement('br');
+//   jokeContainer.appendChild(item);
+//   jokeContainer.appendChild(deleteButton);
+//   jokeContainer.appendChild(br);
+
+//   // }
+// }
 
 function getSavedJokes() {
-  for (let [key, value] of Object.entries(localStorage)) {
-    
-    let item = document.createElement("p");
-    item.innerText = value;
-    item.classList.add("saved-joke");
-    let br = document.createElement("br");
-    jokeContainer.appendChild(item);
-    jokeContainer.appendChild(br);
+  let savedJokes = localStorage.getItem("jokes");
 
-   
+  if (!savedJokes) {
+    return false;
+  }
+
+  savedJokes = JSON.parse(savedJokes);
+
+  console.log(savedJokes)
+  console.log("hereeee")
+
+  for (let i = 0; i < savedJokes.length; i++) {
+    createSingleLikedJoke(savedJokes[i])
   }
 }
+
+function saveJokes() {
+  localStorage.setItem("jokes", JSON.stringify(jokes))
+}
+
+function createSingleLikedJoke(joke_text) {
+  // Create list item
+  let jokeItemEl = document.createElement('li');
+
+  // jokeItemEl.innerText = value;
+  jokeItemEl.className = 'saved-joke';
+  // Custom id for each joke
+  jokeItemEl.setAttribute('data-joke-id', jokeId);
+  // let deleteButton = document.createElement('button');
+  // deleteButton.innerHTML = 'Delete';
+
+  // Create a div to hold the joke and add delete button
+  var singleJokeContainer = document.createElement("div");
+  singleJokeContainer.className = "joke"
+  singleJokeContainer.innerHTML = "<h3 class='whole-joke'>" +
+    joke_text + "</h3>"
+
+    jokeItemEl.appendChild(singleJokeContainer);
+  // let br = document.createElement('br');
+  let deleteButton = createDeleteButton(jokeId);
+  jokeItemEl.appendChild(deleteButton);
+  jokeContainer.appendChild(jokeItemEl);
+  
+  // jokeContainer.appendChild(br);
+  joke_text.id = jokeId;
+  jokes.push(joke_text)
+
+  createSingleLikedJoke();
+  jokeId++;
+}
+
+function createDeleteButton(id) {
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "delete-btn";
+  deleteButton.setAttribute("data-joke-id", id);
+  return deleteButton;
+}
+
+function deleteJoke(id) {
+  localStorage.removeItem(id);
+}
+
 getSavedJokes();
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,95 +3,62 @@ const jokeButton = document.getElementById('joke-btn');
 const saveButton = document.getElementById('save-btn');
 const copyButton = document.getElementById('copy-btn');
 const messageBody = document.getElementById('message-body');
-const getJoke = async () => {
+let currentJoke = document.querySelector('has-text-info');
+let jokes = [];
+let index = 0;
+let jokeId = 0;
+
+// Creates new joke from api call on homepage
+const getNewJoke = async () => {
   const url = 'https://icanhazdadjoke.com/';
   try {
     const response = await fetch(url, { method: 'GET', headers: { accept: 'application/json' } });
 
     let data = await response.json();
     clearContainer();
-    let joke = splitJoke(data.joke);
-    // let dividedJoke = formatJoke(joke);
-    console.log(joke);
-    appendJoke(joke);
+    console.log(data);
+    appendJoke(data.joke);
   } catch (error) {
     console.log(error);
   }
 };
 
+// Clears the current joke on the homepage
 const clearContainer = () => {
   while (jokeContainer.firstChild) {
     jokeContainer.firstChild.remove();
   }
 };
 
-// const formatJoke = (opening, punchLine) => {
-//    console.log('shit')
-//   return [opening, punchLine]
-// }
-
+// Places joke text on screen
 const appendJoke = (apiJoke) => {
   const joke = document.createElement('p');
   console.log('here');
   joke.textContent = apiJoke;
-  joke.classList.add('has-text-info');
-  joke.classList.add('toCopy');
+  joke.className = 'has-text-info toCopy';
+  console.log(joke);
+  currentJoke = joke;
+  console.log(currentJoke)
   jokeContainer.appendChild(joke);
 };
 
-const splitJoke = (joke) => {
-  console.log(joke);
-  // if(joke.includes(':')) {
-  //   console.log('skip')
-  //   getJoke()
-  // }
-
-  let newJoke = joke.split();
-
-  if (newJoke[newJoke.length - 1] === '') {
-    console.log('pop' + newJoke.pop());
-  }
-  if (newJoke.length > 2) {
-    getJoke();
-  }
-  console.log(newJoke);
-  // if(newJoke.length === 2 //|| (newJoke.length === 3 && newJoke[2] === '')//|| newJoke[1] !== undefined
-  //   ){
-  //   console.log('here')
-  // let opening = newJoke[0];
-
-  // let punchLine = newJoke[1]
-  // const formattedJoke = formatJoke(opening, punchLine)// [opening, punchLine]
-  // return formattedJoke
-  // }
-  // else {
-  //   // if (newJoke.includes(''))
-  //   // console.log('undefined')
-  //   // else console.log('too many')
-  //   // return newJoke
-  //    getJoke();
-  // }
-  return newJoke;
-};
-
-const saveJoke = () => {
-
-  console.log(jokeContainer.innerText)
-  window.localStorage.setItem(window.localStorage.length - 1, jokeContainer.innerText);
-};
-
-const copyJoke = () => {
-
-  let message = document.getElementsByClassName('toCopy')[0].innerHTML
-  console.log(typeof message);
-  navigator.clipboard.writeText(message).then(() => {
-    alert("Copied!")
-  })
+// Saves a liked joke to localStorage
+function saveJokes() {
   
-
+  jokes.push(currentJoke.textContent)
+  console.log(jokes)
+  localStorage.setItem('jokes', JSON.stringify(jokes));
 }
 
-jokeButton.addEventListener('click', getJoke);
-saveButton.addEventListener('click', saveJoke);
+// Copies the text of the joke to the clipboard
+const copyJoke = () => {
+  let message = currentJoke.textContent;
+  navigator.clipboard.writeText(message).then(() => {
+    alert('Copied!');
+  });
+};
+
+jokeButton.addEventListener('click', getNewJoke);
+saveButton.addEventListener('click', saveJokes);
 copyButton.addEventListener('click', copyJoke);
-// getJoke();
+//getNewJoke();
